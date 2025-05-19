190116248cf6debf573249863fa24dd56410f7d8
budujoseph <budu.joseph95@gmail.com>
2025-05-19 21:26:58 +0000

    fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction1: {fraction1}");
+
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine($"Fraction1: {fraction2}");
+
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine($"Fraction3: {fraction3}");
+
+        Fraction bottom1 = new Fraction();
+        bottom1.SetBottom(5);
+        Console.WriteLine(bottom1.GetFractionString());
+        Console.WriteLine(bottom1.GetDecimalValue());
+
+
+        // Fraction top1 = new Fraction();
+        // top1.SetTop(4);
+        // Console.WriteLine(top1.GetTop());
+
+
+
+
     }
 }
