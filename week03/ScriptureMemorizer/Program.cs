476dbb5f9347141b6cd023be20ee810f5229101f
budujoseph <budu.joseph95@gmail.com>
2025-05-24 20:35:52 +0000

    scripture mastery done

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.Write("Enter the book name: ");
+        string book = Console.ReadLine();
+
+        Console.Write("Enter the chapter number: ");
+        int chapter = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the verse number: ");
+        int verse = int.Parse(Console.ReadLine());
+
+        Reference reference = new Reference(book, chapter, verse);
+
+        Console.Write("Enter the scripture text: ");
+        string scriptureText = Console.ReadLine();
+
+        Scripture scripture = new Scripture(reference, scriptureText);
+        
+        while (true)
+        {
+            scripture.Display();
+            Console.WriteLine("Press Enter to hide words or press 'quite' to exit. ");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quite")
+            {
+                break;
+            }
+
+            scripture.HideWords(3);
+
+            if (scripture.AllWordsHidden())
+            {
+                scripture.Display();
+                Console.WriteLine("Congratulation! you have memorized the scripture");
+                break;
+            }
+        }
+
+        Console.WriteLine("Thank You!");
     }
 }
