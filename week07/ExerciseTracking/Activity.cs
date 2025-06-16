1b3256fc9d2eac401a5389d5ea4e28d968022983
budujoseph <budu.joseph95@gmail.com>
2025-06-16 19:59:08 +0000

    exercise tracking program

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Activity
+{
+    private DateTime _date;
+    private int _duration;
+
+    public Activity(DateTime date, int duration)
+    {
+        _date = date;
+        _duration = duration;
+    }
+
+    public int Duration => _duration;
+    public DateTime Date => _date;
+
+    public virtual double GetDistance() => 0;
+    public virtual double GetSpeed() => 0;
+    public virtual double GetPace() => 0;
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToString("dd MMM yyyy")} Activity ({_duration} min) : Distance {GetDistance(): 0.0} km, Speed {GetSpeed(): 0.0} kph, Pace: {GetPace(): 0.0} min per km";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2023, 03, 4), 30, 4.8),
+            new Cycling(new DateTime(2023, 12, 11), 25, 25.0),
+            new Swimming(new DateTime(2023, 10, 05), 30, 14)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
